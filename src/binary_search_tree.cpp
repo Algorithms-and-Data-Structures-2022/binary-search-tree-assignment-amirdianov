f80c29fc4c76618b547cb79493c3b7521320ed31
diyanovamir <amirdianov@gmail.com>
2022-03-25 19:14:45 +0300

    All done!

--- src/binary_search_tree.cpp
+++ src/binary_search_tree.cpp
@@ -85,8 +85,53 @@ namespace assignment {
   }
 
   bool BinarySearchTree::remove(int key, Node*& node) {
-    //code
+    if (node == nullptr) {
+      // узел не найден
+      return false;
     }
+
+    if (key == node->key) {
+
+      // Случай 1. Два потомка
+      if (node->left != nullptr && node->right != nullptr) {
+
+        // наименьший узел в правом поддереве
+        // пояснение:
+        //  1. в правом поддереве любой элемент гарантированно больше
+        //  любого элемента из левого поддерева
+        //  2. на месте удаляемого узла должен стоять элемент:
+        //    2.1. значение элемента > любого элемента из левого поддерева
+        //    2.2. значение элемента < любого элемента из правого поддерева
+        Node* min = find_min(node->right);
+
+        node->key = min->key;
+        node->value = min->value;
+
+        return remove(min->key, node->right);
+      }
+
+      // Случай 2. Левый потомок
+      if (node->left != nullptr && node->right == nullptr) {
+        Node* left_child = node->left;
+        delete node;
+        node = left_child;
+        return true;
+      }
+
+      // Случай 3. Правый потомок или нет потомков
+      Node* right_node = node->right;
+      delete node;
+      node = right_node;
+      return true;
+    }
+
+    if (key < node->key) {
+      return remove(key, node->left);
+    }
+
+    // key > node->key
+    return remove(key, node->right);
+  }
 
   void BinarySearchTree::clear(Node* node) {
     if (node != nullptr) {
@@ -113,11 +158,17 @@ namespace assignment {
   }
 
   Node* BinarySearchTree::find_min(Node* node) const {
-    return nullptr;
+    while (node->left != nullptr){
+      node = node->left;
+    }
+    return node;
   }
 
   Node* BinarySearchTree::find_max(Node* node) const {
-    return nullptr;
+    while (node->right != nullptr){
+      node = node->right;
+    }
+    return node;
   }
 
 }  // namespace assignment
